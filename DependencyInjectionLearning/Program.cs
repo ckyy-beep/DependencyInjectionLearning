c00c7af68dd68f67ec6b83a9052b07cc2fc35104
Chris Yeung <jackknife29jk@gmail.com>
2023-09-18 14:20:17 +0200

    first commit
    some code - need to fix

--- DependencyInjectionLearning/Program.cs
+++ DependencyInjectionLearning/Program.cs
@@ -1,2 +1,37 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+namespace propertyInjection
+{
+    public interface Text
+    {
+        void print();
+    }
+    class format : Text
+    {
+        public void print()
+        {
+            Console.WriteLine("Hello world! play valorant!");
+        }
+    }
+    public class constructorinjection
+    {
+        private Text _text;
+        public constructorinjection(Text t1)
+        {
+            this._text = t1;
+        }
+        public void print()
+        {
+            _text.print();
+        }
+    }
+
+    class constructor
+    {
+        static void main(String[] args)
+        {
+            constructorinjection cs = new constructorinjection(BinaryWriter format());
+            cs.print();
+            Console.ReadKey();
+        }
+    }
+}
